ec88a51e832f337cf884b4f890a872a13248e065
rootalex <rootalex.n@gmail.com>
2020-12-22 20:05:52 +0200

    Create shed10min.py

---
+++ shed10min.py
@@ -0,0 +1,19 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import urllib.request
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=1)
+def timed_job():
+    # print('This job is run every three minutes.')
+    webUrl = urllib.request.urlopen('https://etcet-test.herokuapp.com/done_to_todo')
+    # print("result code: " + str(webUrl.getcode()))
+    # print(webUrl.read())
+
+
+'''
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=19, minute=37)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
+'''
+sched.start()
